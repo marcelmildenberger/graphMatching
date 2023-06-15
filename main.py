0f4a31bbba19a5c1d7f310f8ace93d18e16352c1
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-06-15 16:55:04 +0200

    Extended example code

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import csv
 import numpy as np
-import pandas as pd
 
 from encoders.bf_encoder import BFEncoder
 from embedders.node2vec import N2VEmbedder
 
-alice = pd.read_csv("./data/feb14.csv")
-alice = alice.head(1000)
-alice.replace(np.nan, "", inplace=True)
+# Some global parameters
 
-alice = alice[["given_name", "surname", "address_2"]]
-alice = [[str(row[1]["given_name"]), str(row[1]["surname"]), str(row[1]["address_2"])] for row in alice.iterrows()]
+# Configuration for Bloom filters
+BF_CONFIG = {
+    "AliceData": "./data/feb14.csv",
+    "AliceSecret": "SuperSecretSalt1337",
+    "AliceBFLength": 1024,
+    "AliceBits": 30,
+    "AliceN": 2,
+    "EveData": "./data/feb14.csv",
+    "EveSecret": "ATotallyDifferentString",
+    "EveBFLength": 1024,
+    "EveBits": 30,
+    "eveN": 2,
+}
+
+# Load and encode Alice's Data
 
 alice_bloom_encoder = BFEncoder("secret", 1024, 30, 2)
 
@@ -21,7 +31,7 @@ alice_sims = [e[2] for e in alice_enc]
 tres = np.quantile(alice_sims, 0.99)
 alice_minsim = [e for e in alice_enc if e[2]>tres]
 
-with open("./data/testfile.edg", "w") as f:
+with open("./data/testfile.edg", "w", newline="") as f:
     csvwriter = csv.writer(f, delimiter="\t")
     csvwriter.writerows(alice_minsim)
 
@@ -29,3 +39,6 @@ n2v_embedder = N2VEmbedder(walk_length=100, n_walks=20, p=0.5, q=2, dim_embeddin
                            seed=42, workers=6)
 
 n2v_embedder.train("./data/testfile.edg")
+n2v_embedder.save_model()
+
+embeddings = n2v_embedder.get_vectors()
