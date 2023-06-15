284b1fdc72e2e80b726efc8cdb1b91cb45fdee04
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-06-15 15:36:55 +0200

    Bugfixes

--- encoders/encoder.py
+++ encoders/encoder.py
@@ -1,15 +1,10 @@
 # Specifies an interface that must be implemented by encoders in order to be used in the graph matching attack
 import numpy as np
 from abc import ABC, abstractmethod
-from typing import Sequence
+from typing import Sequence, List, Tuple
 class Encoder(ABC):
 
     @abstractmethod
-    def encode_and_compare(self, data: Sequence[Sequence[str]]) -> np.ndarray:
-        """
-        Encodes the passed data using an encoding algorithm and returns a numpy array containing the
-        pairwise similarities of the records.
-        :param data: The data to encode as a list of lists: [["Rec1Attr1","Rec1Attr2"],["Rec2Attr1","Rec2Attr2"]]
-        :return: A numpy array with the pairwise similarities of the encoded records
-        """
-        return np.ndarray()
+    def encode_and_compare(self, data: Sequence[Sequence[str]]) -> List[Tuple[int, int, float]]:
+
+        return [(1, 1, 1), (2, 2, 2)]
