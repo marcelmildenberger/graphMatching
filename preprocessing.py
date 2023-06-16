5d89a25a7cff43bdd60286277ade95f1a3056a3d
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-06-16 15:03:14 +0200

    Shifted to Name + Birthdate instead of address

--- preprocessing.py
+++ preprocessing.py
@@ -4,9 +4,9 @@ import numpy as np
 from typing import Sequence
 
 data = pd.read_csv("./data/feb14.csv")
-data = data.head(1000)
-data.replace(np.nan, "", inplace=True)
-data = data[["given_name", "surname", "address_2"]]
+#data = data.head(1000)
+data.replace(np.nan, "nan", inplace=True)
+data = data[["given_name", "surname", "date_of_birth"]]
 
 data.to_csv("./data/alice.csv", index=False)
 data.to_csv("./data/eve.csv", index=False)
