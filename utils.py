384d3b4493c2a7736e9508e382860bd5c7cdbbac
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-06-19 11:40:05 +0200

    Default delimiter and dict mode

--- utils.py
+++ utils.py
@@ -2,17 +2,22 @@ import csv
 from typing import Sequence
 
 
-def read_csv(path: str, header: bool = True) -> Sequence[Sequence[str]]:
-    data = []
+def read_csv(path: str, header: bool = True, as_dict: bool = False, delim: str = "\t") -> Sequence[Sequence[str]]:
+    data = {} if as_dict else []
     with open(path, "r") as f:
-        reader = csv.reader(f)
+        reader = csv.reader(f, delimiter=delim)
         if header:
             next(reader)
         for row in reader:
-            data.append(row)
+            if as_dict:
+                assert len(row) == 2, "Dict mode only supports rows with two lines"
+                data[row[0]] = row[1]
+            else:
+                data.append(row)
     return data
 
-def save_csv(data, path: str):
+
+def save_csv(data, path: str, delim: str = "\t"):
     with open(path, "w", newline="") as f:
-        csvwriter = csv.writer(f, delimiter="\t")
+        csvwriter = csv.writer(f, delimiter=delim)
         csvwriter.writerows(data)
