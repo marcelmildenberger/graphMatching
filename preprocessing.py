d681f4d5c28195e86ea06ce0ba7ddbb5b02a2d9a
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-06-19 11:39:52 +0200

    Handling of NAs changed

--- preprocessing.py
+++ preprocessing.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 import random
 
-from utils import save_csv
+from utils import save_csv, read_csv
 
 OVERLAP = 0.5
 
 data = pd.read_csv("./data/feb14.csv")
 #data = data.head(1000)
-data.replace(np.nan, "nan", inplace=True)
 data = data[["given_name", "surname", "date_of_birth"]]
-
+# data.replace(np.nan, "NA", inplace=True)
+# data = data.astype({'date_of_birth': 'Int64'})
+data = data.dropna()
+data = data.astype({'date_of_birth': 'int'})
 eve = data
 
 # Creates a list of the indexes in Eve's dataset
@@ -25,10 +27,11 @@ alice = eve.iloc[ind]
 ground_truth = [("E_"+str(ind[i]), "A_"+str(i)) for i in range(len(ind))]
 
 # Save data
-alice.to_csv("./data/alice.csv", index=False)
-eve.to_csv("./data/eve.csv", index=False)
+alice.to_csv("./data/alice.tsv", index=False, sep="\t")
+eve.to_csv("./data/eve.tsv", index=False, sep = "\t")
 save_csv(ground_truth, "./data/eve_to_alice.tsv")
 
+test = read_csv("./data/eve_to_alice.tsv", header=False, as_dict=True)
 print("Done")
 
 
