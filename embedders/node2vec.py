a15a788f01ab328b15f63cd40b5cf1e51a69581a
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-07-19 11:55:57 +0200

    DevMode and Logging for N2V

--- embedders/node2vec.py
+++ embedders/node2vec.py
@@ -1,10 +1,31 @@
 import os
+import pickle
+
 import numpy as np
 import pecanpy
 
 from typing import List, Tuple, Union
 from gensim.models import Word2Vec
 from datetime import datetime
+
+from gensim.models.callbacks import CallbackAny2Vec
+
+
+class LossLogger(CallbackAny2Vec):
+    '''Output loss at each epoch'''
+    def __init__(self):
+        self.epoch = 1
+        self.losses = [0]
+
+    def on_epoch_begin(self, model):
+        print(f'Epoch: {self.epoch}', end='\t')
+
+    def on_epoch_end(self, model):
+        loss = model.get_latest_training_loss()
+        self.losses.append(loss)
+        print(f'  Loss: {loss-self.losses[self.epoch-1]}')
+        self.epoch += 1
+
 class N2VEmbedder():
 
     def __init__(self, walk_length: int, n_walks: int, p: int, q: int, dim_embeddings: int, context_size: int, epochs,
@@ -21,17 +42,35 @@ class N2VEmbedder():
 
         self.model = None
 
-    def train(self, data_dir: str):
-        graph = pecanpy.pecanpy.SparseOTF(p=self.p, q=self.q, workers=self.workers, verbose=True, random_state=self.seed)
+        self.dev = False
+        self.load_stored = False
 
+    def train(self, data_dir: str):
+        graph = pecanpy.pecanpy.SparseOTF(p=self.p, q=self.q, workers=self.workers, verbose=True, random_state=self.seed,
+                                          extend=True)
+
+        datname = "alice" if "alice" in data_dir else "eve"
         # Read the data  into the graph.
         graph.read_edg(data_dir, weighted=True, directed=False)
         # Generate the random walks
-        walks = graph.simulate_walks(num_walks=self.n_walks, walk_length=self.walk_length)
+        if self.load_stored:
+            with open("./dev/walks_"+datname+".pck", "rb") as f:
+                walks = pickle.load(f)
+        else:
+            walks = graph.simulate_walks(num_walks=self.n_walks, walk_length=self.walk_length)
+
+        if self.dev:
+            with open("./dev/walks_"+datname+".pck", "wb") as f:
+                pickle.dump(walks, f)
+
+
         # Run Word2Vec on the random walk to generate node2vec embeddings
+        loss_logger = LossLogger()
         self.model = Word2Vec(
             walks, vector_size=self.dim_embeddings, window=self.context_size / 2, min_count=0, sg=1,
-            workers=self.workers, epochs=self.epochs)
+            workers=self.workers, epochs=self.epochs, compute_loss=True, callbacks=[loss_logger], seed=self.seed)
+        #alpha=0.05, min_alpha=0.01, seed=self.seed)
+
 
     def save_model(self, path=None, filename=None):
         assert self.model is not None, "Model must be trained first"
