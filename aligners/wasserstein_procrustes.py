9c456ca6f65ac829401e62dec1c13c73a9e5156a
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-09-26 17:49:05 +0200

    GPU Support for Wasserstein Alignment

--- aligners/wasserstein_procrustes.py
+++ aligners/wasserstein_procrustes.py
@@ -9,24 +9,25 @@
 
 import time, math, ot
 import numpy as np
+import torch
 from tqdm import trange
 
 
 def sqrt_eig(x):
-    U, s, VT = np.linalg.svd(x, full_matrices=False)
-    return np.dot(U, np.dot(np.diag(np.sqrt(s)), VT))
+    U, s, VT = torch.linalg.svd(x, full_matrices=False)
+    return torch.matmul(U, torch.matmul(torch.diag(torch.sqrt(s)), VT))
 
 
 def procrustes(X_src, Y_tgt):
-    U, s, V = np.linalg.svd(np.dot(Y_tgt.T, X_src))
-    return np.dot(U, V)
+    U, s, V = torch.linalg.svd(torch.matmul(Y_tgt.T, X_src))
+    return torch.matmul(U, V)
 
 
 class WassersteinAligner:
 
     def __init__(self, reg_init, reg_ws, batchsize, lr, n_iter_init, n_iter_ws, n_epoch, vocab_size, lr_decay,
                  apply_sqrt, early_stopping, seed=42, verbose=True, min_epsilon=0.0005):
-        #np.random.seed(seed)
+        # np.random.seed(seed)
         self.reg_init = reg_init
         self.reg_ws = reg_ws
         self.batchsize = batchsize
@@ -43,12 +44,23 @@ class WassersteinAligner:
 
         self.X = None
         self.Y = None
+        self.use_gpu = False
+        if torch.cuda.is_available():
+            self.use_gpu = True
+            if self.verbose:
+                for i in range(torch.cuda.device_count()):
+                    print("Found GPU " + torch.cuda.get_device_properties(i).name)
+                print("Will use GPU for acceleration.")
 
     def objective(self, R, n=1000):
         Xn, Yn = self.X[:n], self.Y[:n]
-        C = -np.dot(np.dot(Xn, R), Yn.T)
-        P = ot.sinkhorn(np.ones(n), np.ones(n), C, 0.9, stopThr=1e-3, numItermax=1000)
-        return 1000 * np.linalg.norm(np.dot(Xn, R) - np.dot(P, Yn)) / n
+        C = -torch.matmul(torch.matmul(Xn, R), Yn.T)
+        if self.use_gpu:
+            P = ot.sinkhorn(torch.ones(n, device='cuda'), torch.ones(n, device='cuda'), C, 0.9, stopThr=1e-3,
+                            numItermax=1000)
+        else:
+            P = ot.sinkhorn(torch.ones(n), torch.ones(n), C, 0.9, stopThr=1e-3, numItermax=1000)
+        return 1000 * torch.linalg.norm(torch.matmul(Xn, R) - torch.matmul(P, Yn)) / n
 
     def solve_procrustes(self, R):
         assert self.X is not None and self.Y is not None, "Matrices must not be empty!"
@@ -65,21 +77,28 @@ class WassersteinAligner:
                 return best_R
             for _it in trange(1, self.n_iter_ws + 1, desc="Iteration", disable=not self.verbose):
                 # sample mini-batch
-                xt = self.X[np.random.permutation(self.X.shape[0])[:self.batchsize], :]
-                yt = self.Y[np.random.permutation(self.Y.shape[0])[:self.batchsize], :]
+                xt = self.X[torch.randperm(len(self.X))[:self.batchsize]]
+                yt = self.Y[torch.randperm(len(self.Y))[:self.batchsize]]
                 # compute OT on minibatch
-                C = -np.dot(np.dot(xt, R), yt.T)
-                P = ot.sinkhorn(np.ones(self.batchsize), np.ones(self.batchsize), C, self.reg_ws, stopThr=1e-3,
-                                numItermax=1000)
+                C = -torch.matmul(torch.matmul(xt, R), yt.T)
+                if self.use_gpu:
+                    P = ot.sinkhorn(torch.ones(self.batchsize, device='cuda'),
+                                    torch.ones(self.batchsize, device='cuda'), C, self.reg_ws, stopThr=1e-3,
+                                    numItermax=1000)
+                else:
+                    P = ot.sinkhorn(torch.ones(self.batchsize), torch.ones(self.batchsize), C, self.reg_ws,
+                                    stopThr=1e-3,
+                                    numItermax=1000)
                 # compute gradient
-                G = - np.dot(xt.T, np.dot(P, yt))
+                G = - torch.matmul(xt.T, torch.matmul(P, yt))
                 R -= self.lr / self.batchsize * G
                 # project on orthogonal matrices
-                U, s, VT = np.linalg.svd(R)
-                R = np.dot(U, VT)
+                U, s, VT = torch.linalg.svd(R)
+                R = torch.matmul(U, VT)
             self.lr *= self.lr_decay
             self.lr = max(self.lr, 1)
 
+            print("Objective")
             obj = self.objective(R, n=min(self.Y.shape[0], self.X.shape[0]))
 
             if first_obj == -1:
@@ -102,52 +121,61 @@ class WassersteinAligner:
 
     def convex_init(self, X=None, Y=None):
         if X is not None or Y is not None:
-            self.X = X
-            self.Y = Y
+            self.X = torch.from_numpy(X)
+            self.Y = torch.from_numpy(Y)
 
         # If the two matrices contain a different number of records, reduce the size to the smaller of the two
         # by random subsampling.
 
         if min(self.X.shape[0], self.Y.shape[0]) > 10000:
-            X_c = self.X[np.random.permutation(self.X.shape[0])[:10000], :]
-            Y_c = self.Y[np.random.permutation(self.Y.shape[0])[:10000], :]
+            X_c = self.X[torch.randperm(len(self.X))[:10000]]
+            Y_c = self.Y[torch.randperm(len(self.Y))[:10000]]
         else:
-            if self.X.shape[0] > self.Y.shape[0]: #<
+            if self.X.shape[0] > self.Y.shape[0]:  # <
                 X_c = self.X
-                #Y_c = self.Y[np.random.permutation(self.Y.shape[0])[:self.X.shape[0]], :]
-                Y_c = np.vstack([self.Y, self.Y[np.random.choice(self.Y.shape[0], self.X.shape[0] - self.Y.shape[0], replace=True), :]])
-            elif self.X.shape[0] < self.Y.shape[0]: #>
-                #X_c = self.X[np.random.permutation(self.X.shape[0])[:self.Y.shape[0]], :]
-                X_c = np.vstack([self.X, self.X[np.random.choice(self.X.shape[0], self.Y.shape[0] - self.X.shape[0], replace=True), :]])
+                Y_c = torch.vstack((self.Y, self.Y[torch.randint(len(self.Y), (self.X.shape[0] - self.Y.shape[0],))]))
+            elif self.X.shape[0] < self.Y.shape[0]:  # >
+                X_c = torch.vstack((self.X, self.X[torch.randint(len(self.X), (self.Y.shape[0] - self.X.shape[0],))]))
                 Y_c = self.Y
             else:
                 X_c = self.X
                 Y_c = self.Y
 
-
         n, d = X_c.shape
 
         if self.apply_sqrt:
             X_c, Y_c = sqrt_eig(X_c), sqrt_eig(Y_c)
-        K_X, K_Y = np.dot(X_c, X_c.T), np.dot(Y_c, Y_c.T)
-        K_Y *= np.linalg.norm(K_X) / np.linalg.norm(K_Y)
-        K2_X, K2_Y = np.dot(K_X, K_X), np.dot(K_Y, K_Y)
-        P = np.ones([n, n]) / float(n)
+
+        K_X, K_Y = torch.matmul(X_c, X_c.T), torch.matmul(Y_c, Y_c.T)
+        K_Y *= torch.linalg.norm(K_X) / torch.linalg.norm(K_Y)
+        K2_X, K2_Y = torch.matmul(K_X, K_X), torch.matmul(K_Y, K_Y)
+        P = torch.ones(n, n) / float(n)
+        if self.use_gpu:
+            P = P.to('cuda')
         for it in trange(1, self.n_iter_init + 1, disable=not self.verbose):
-            G = np.dot(P, K2_X) + np.dot(K2_Y, P) - 2 * np.dot(K_Y, np.dot(P, K_X))
-            q = ot.sinkhorn(np.ones(n), np.ones(n), G, self.reg_init, stopThr=1e-4)
+            G = torch.matmul(P, K2_X) + torch.matmul(K2_Y, P) - 2 * torch.matmul(K_Y, torch.matmul(P, K_X))
+            if self.use_gpu:
+                q = ot.sinkhorn(torch.ones(n, device='cuda'), torch.ones(n, device='cuda'), G, self.reg_init,
+                                stopThr=1e-4)
+            else:
+                q = ot.sinkhorn(torch.ones(n), torch.ones(n), G, self.reg_init, stopThr=1e-4)
             alpha = 2.0 / float(2.0 + it)
             P = alpha * q + (1.0 - alpha) * P
-        R0 = procrustes(np.dot(P, X_c), Y_c).T
+        R0 = procrustes(torch.matmul(P, X_c), Y_c).T
         obj = self.objective(R0, n=min(self.Y.shape[0], self.X.shape[0]))
-        #obj = np.linalg.norm(np.dot(P, K_X) - np.dot(K_Y, P))
+        # obj = np.linalg.norm(np.dot(P, K_X) - np.dot(K_Y, P))
         if self.verbose:
             print("Objective after convex initialization: %f" % obj)
         return R0, obj
 
     def align(self, src, tgt):
-        self.X = src
-        self.Y = tgt
+
+        self.X = torch.from_numpy(src)
+        self.Y = torch.from_numpy(tgt)
+
+        if self.use_gpu:
+            self.X = self.X.to('cuda')
+            self.Y = self.Y.to('cuda')
 
         if self.verbose:
             print("\nComputing initial mapping with convex relaxation...")
@@ -162,4 +190,4 @@ class WassersteinAligner:
         if self.verbose:
             print("Done [%03d sec]" % math.floor(time.time() - t0))
 
-        return R
+        return R.numpy(force=True)
