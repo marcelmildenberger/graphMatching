ce5ab8e71a3057934cb1daf49219021a60e341e0
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-11-09 12:08:17 +0100

    Optimisations

--- aligners/wasserstein_procrustes.py
+++ aligners/wasserstein_procrustes.py
@@ -25,7 +25,7 @@ def procrustes(X_src, Y_tgt):
 class WassersteinAligner:
 
     def __init__(self, reg_init, reg_ws, batchsize, lr, n_iter_init, n_iter_ws, n_epoch, vocab_size, lr_decay,
-                 apply_sqrt, early_stopping, seed=42, verbose=True, min_epsilon=0.0005):
+                 apply_sqrt, early_stopping, seed=42, verbose=True, min_epsilon=0.005):
         # np.random.seed(seed)
         self.reg_init = reg_init
         self.reg_ws = reg_ws
@@ -57,7 +57,7 @@ class WassersteinAligner:
         C = -torch.matmul(torch.matmul(Xn, R), Yn.T)
         if self.use_gpu:
             P = ot.sinkhorn(torch.ones(n, device='cuda'), torch.ones(n, device='cuda'), C, 0.9, stopThr=1e-3,
-                            numItermax=1000, method = "sinkhorn_log")
+                            numItermax=1000, method="sinkhorn_log")
         else:
             P = ot.sinkhorn(torch.ones(n), torch.ones(n), C, 0.9, stopThr=1e-3, numItermax=1000)
         return 1000 * torch.linalg.norm(torch.matmul(Xn, R) - torch.matmul(P, Yn)) / n
@@ -66,47 +66,66 @@ class WassersteinAligner:
         assert self.X is not None and self.Y is not None, "Matrices must not be empty!"
         no_improvement = 0
         first_obj = -1
+        t = 0.5
         prev_obj = float("inf")
         best_obj = float("inf")
         best_R = R
+
         for epoch in range(1, self.n_epoch + 1):
             if self.early_stopping > 0 and no_improvement >= self.early_stopping:
                 if self.verbose:
-                    print("Objective didn't improve for %i epochs. Stopping..." % self.early_stopping)
+                    print("Objective didn't improve for %i epochs. Early Stopping..." % self.early_stopping)
                     print("Improvement: %f" % (first_obj - best_obj))
-                return best_R
+                return best_R, best_obj, False
+
             for _it in trange(1, self.n_iter_ws + 1, desc="Iteration", disable=not self.verbose):
                 # sample mini-batch
                 xt = self.X[torch.randperm(len(self.X))[:self.batchsize]]
                 yt = self.Y[torch.randperm(len(self.Y))[:self.batchsize]]
+
                 # compute OT on minibatch
                 C = -torch.matmul(torch.matmul(xt, R), yt.T)
+
                 if self.use_gpu:
                     P = ot.sinkhorn(torch.ones(self.batchsize, device='cuda'),
-                                    torch.ones(self.batchsize, device='cuda'), C, self.reg_ws, stopThr=1e-3,
-                                    numItermax=1000, method = "sinkhorn_log")
+                                    torch.ones(self.batchsize, device='cuda'), C, self.reg_ws, stopThr=t,
+                                    numItermax=100000, method="sinkhorn_log")
+
                 else:
                     P = ot.sinkhorn(torch.ones(self.batchsize), torch.ones(self.batchsize), C, self.reg_ws,
-                                    stopThr=1e-3,
-                                    numItermax=1000)
+                                    stopThr=t,
+                                    numItermax=100000)
+
                 # compute gradient
                 G = - torch.matmul(xt.T, torch.matmul(P, yt))
                 R -= self.lr / self.batchsize * G
                 # project on orthogonal matrices
                 U, s, VT = torch.linalg.svd(R)
                 R = torch.matmul(U, VT)
+                if first_obj == -1:
+                    obj = 0
+                    for i in range(5):
+                        obj += self.objective(R, n=min(20000, min(self.Y.shape[0], self.X.shape[0])))
+
+                    obj /= 5
+                    first_obj = obj
+
             self.lr *= self.lr_decay
-            self.lr = max(self.lr, 1)
+            self.lr = max(self.lr, 5)
 
             obj = 0
             for i in range(5):
-                obj += self.objective(R, n=min(self.Y.shape[0], self.X.shape[0]))
+                obj += self.objective(R, n=min(20000, min(self.Y.shape[0], self.X.shape[0])))
 
             obj /= 5
 
-            if first_obj == -1:
-                first_obj = obj
             if obj < best_obj:
+                improved_ratio = obj / first_obj
+                if best_obj != float("inf") and improved_ratio < 0.9:
+                    if self.verbose:
+                        print("Objective of  %.3f is %2.2f %% of initial value. Early stopping..." % (
+                        obj, improved_ratio * 100))
+                    return R, obj, True
                 best_obj = obj
                 best_R = R
 
@@ -120,7 +139,7 @@ class WassersteinAligner:
                     no_improvement = 0
                 prev_obj = best_obj
 
-        return best_R
+        return best_R, best_obj, False
 
     def convex_init(self, X=None, Y=None):
         if X is not None or Y is not None:
@@ -130,19 +149,21 @@ class WassersteinAligner:
         # If the two matrices contain a different number of records, reduce the size to the smaller of the two
         # by random subsampling.
 
-        if min(self.X.shape[0], self.Y.shape[0]) > 10000:
-            X_c = self.X[torch.randperm(len(self.X))[:10000]]
-            Y_c = self.Y[torch.randperm(len(self.Y))[:10000]]
+        if self.X.shape[0] < self.Y.shape[0]:  # <
+            X_c = self.X
+            Y_c = self.Y[torch.randperm(len(self.Y))[:self.X.shape[0]]]
+        elif self.X.shape[0] > self.Y.shape[0]:  # >
+            X_c = self.X[torch.randperm(len(self.X))[:self.Y.shape[0]]]
+            Y_c = self.Y
         else:
-            if self.X.shape[0] > self.Y.shape[0]:  # <
-                X_c = self.X
-                Y_c = torch.vstack((self.Y, self.Y[torch.randint(len(self.Y), (self.X.shape[0] - self.Y.shape[0],))]))
-            elif self.X.shape[0] < self.Y.shape[0]:  # >
-                X_c = torch.vstack((self.X, self.X[torch.randint(len(self.X), (self.Y.shape[0] - self.X.shape[0],))]))
-                Y_c = self.Y
-            else:
-                X_c = self.X
-                Y_c = self.Y
+            X_c = self.X
+            Y_c = self.Y
+
+        if X_c.shape[0] > 20000:
+            X_c = X_c[torch.randperm(len(X_c))[:20000]]
+
+        if Y_c.shape[0] > 20000:
+            Y_c = Y_c[torch.randperm(len(Y_c))[:20000]]
 
         n, d = X_c.shape
 
@@ -159,14 +180,13 @@ class WassersteinAligner:
             G = torch.matmul(P, K2_X) + torch.matmul(K2_Y, P) - 2 * torch.matmul(K_Y, torch.matmul(P, K_X))
             if self.use_gpu:
                 q = ot.sinkhorn(torch.ones(n, device='cuda'), torch.ones(n, device='cuda'), G, self.reg_init,
-                                stopThr=1e-4, method = "sinkhorn_log")
+                                stopThr=3, method="sinkhorn_log", numItermax=10000)
             else:
-                q = ot.sinkhorn(torch.ones(n), torch.ones(n), G, self.reg_init, stopThr=1e-4)
+                q = ot.sinkhorn(torch.ones(n), torch.ones(n), G, self.reg_init, stopThr=3, numItermax=10000)
             alpha = 2.0 / float(2.0 + it)
             P = alpha * q + (1.0 - alpha) * P
         R0 = procrustes(torch.matmul(P, X_c), Y_c).T
-        obj = self.objective(R0, n=min(self.Y.shape[0], self.X.shape[0]))
-        # obj = np.linalg.norm(np.dot(P, K_X) - np.dot(K_Y, P))
+        obj = self.objective(R0, n=min(10000, min(self.Y.shape[0], self.X.shape[0])))
         if self.verbose:
             print("Objective after convex initialization: %f" % obj)
         return R0, obj
@@ -180,16 +200,25 @@ class WassersteinAligner:
             self.X = self.X.to('cuda')
             self.Y = self.Y.to('cuda')
 
-        if self.verbose:
-            print("\nComputing initial mapping with convex relaxation...")
         t0 = time.time()
-        R0, _ = self.convex_init()
-        if self.verbose:
-            print("Done [%03d sec]" % math.floor(time.time() - t0))
-            print("\nComputing mapping with Wasserstein Procrustes...")
 
-        t0 = time.time()
-        R = self.solve_procrustes(R0)
+        best_obj = float("inf")
+        for i in range(10):
+            if self.verbose:
+                print("\nComputing initial mapping with convex relaxation...")
+            t0 = time.time()
+            R0, _ = self.convex_init()
+            if self.verbose:
+                print("Done [%03d sec]" % math.floor(time.time() - t0))
+                print("\nComputing mapping with Wasserstein Procrustes...")
+
+            tmp_r, tmp_obj, success = self.solve_procrustes(R0)
+            if tmp_obj < best_obj:
+                R = tmp_r
+                best_obj = tmp_obj
+            if success:
+                break
+
         if self.verbose:
             print("Done [%03d sec]" % math.floor(time.time() - t0))
         torch.cuda.empty_cache()
