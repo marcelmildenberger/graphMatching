0083ffbdb915e2f068a759604528da8121bc7340
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2023-11-10 15:25:50 +0100

    Changed Data Types to float32

--- encoders/non_encoder.py
+++ encoders/non_encoder.py
@@ -49,7 +49,7 @@ def q_gram_jacc_sim(q_gram_set1, q_gram_set2):
 
 
 def compute_metrics(inds, cache, uids, metric, sim):
-    tmp = np.zeros((len(inds),3), dtype=float)
+    tmp = np.zeros((len(inds),3), dtype=np.float32)
     pos = 0
     prev_i = prev_j = None
     for i, j in inds:
@@ -97,7 +97,7 @@ class NonEncoder():
             cache[uids[i]] = enc
 
         numex = len(uids)
-        output_generator = parallel(delayed(make_inds)(i, numex) for i in np.array_split(np.arange(numex), self.workers * 4))
+        output_generator = parallel(delayed(make_inds)(i, numex) for i in np.array_split(np.arange(numex), self.workers))
         inds = np.vstack(output_generator)
         inds = np.array_split(inds, self.workers)
 
