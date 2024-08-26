b67cc2a6b3c6bf6b4c5152332ce6a1502b736297
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2024-08-26 12:36:54 +0200

    Fixed unused data type

--- embedders/netmf.py
+++ embedders/netmf.py
@@ -120,7 +120,7 @@ class NetMFEmbedder():
             ordering = list(self.indexdict.keys())
             random.shuffle(ordering)
         embeddings = [self.emb_matrix[self.indexdict[k]] for k in ordering]
-        embeddings = np.stack(embeddings, axis=0, dtype=np.float32)
+        embeddings = np.stack(embeddings, axis=0)
         return embeddings, ordering
 
     def get_vector(self, key: Union[int, str]) -> np.ndarray:
