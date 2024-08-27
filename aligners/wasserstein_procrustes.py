e3625fc3ab67a0f5c92e2756a36eefdc9cec4dc8
Jochen Schäfer <jochen.schaefer@uni-mannheim.de>
2024-08-27 19:35:54 +0200

    More Licensing

--- aligners/wasserstein_procrustes.py
+++ aligners/wasserstein_procrustes.py
@@ -1,11 +1,6 @@
 # Based on: https://github.com/facebookresearch/fastText/blob/master/alignment/unsup_align.py
 # and https://github.com/facebookresearch/fastText/blob/master/alignment/utils.py
 
-# Copyright (c) 2018-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
 
 import time, math, ot
 import torch
