932170165842b3e460eab63e0b82fa3076989484
marcelmildenberger <marcel.milde@outlook.de>
2026-03-18 14:52:25 +0100

    cleanup

--- gma.py
+++ gma.py
@@ -6,6 +6,7 @@ from graphMatching.gma_pipeline import (
 
 
 def run_gma(GLOBAL_CONFIG, ENC_CONFIG, EMB_CONFIG, ALIGN_CONFIG, eve_enc_hash, alice_enc_hash, eve_emb_hash, alice_emb_hash):
+    """Run the graph matching attack pipeline for the current experiment."""
     validate_gma_configuration(GLOBAL_CONFIG, ENC_CONFIG, ALIGN_CONFIG)
     similarity_state = prepare_similarity_graph_inputs(
         GLOBAL_CONFIG,
